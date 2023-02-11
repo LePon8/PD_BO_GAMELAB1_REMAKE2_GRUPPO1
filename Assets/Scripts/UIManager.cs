94cbc26318a7e9a9cd034541a32d1961c2f5cf65
LePon <edoardopoma98@gmail.com>
2023-02-11 11:44:09 +0100

    InGame

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,27 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
+    //Controlla se sei in gioco
+    [SerializeField] bool inGame = false;
+
     [SerializeField] GameObject startMenu;
     [SerializeField] GameObject pausa;
+
+    [SerializeField] Text textScore;
+    int score = 1;
+    int maxScore;
+    int minScore;
+
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
         //Disattiva lo script del player cercandolo in scena
         GameObject.Find("Player").GetComponent<Gestione_Player>().enabled = false;
+        
+        //Imposta lo score a zero
+        textScore.text = "0";
     }
 
     // Update is called once per frame
     void Update()
     {
         Pausa();
+        Score();
     }
 
     public void StartGame()
     {
+        //Controlla se sei in gioco
+        inGame = true;
         //Disattiva il menu e riattiva lo script
         startMenu.SetActive(false);
         GameObject.Find("Player").GetComponent<Gestione_Player>().enabled = true;
@@ -34,6 +51,7 @@ public class UIManager : MonoBehaviour
             //Attiva il menu di pausa e disattiva lo script del player
             pausa.SetActive(true);
             GameObject.Find("Player").GetComponent<Gestione_Player>().enabled = false;
+            inGame = false;
            
         }
     }
@@ -43,5 +61,30 @@ public class UIManager : MonoBehaviour
     {
         pausa.SetActive(false);
         GameObject.Find("Player").GetComponent<Gestione_Player>().enabled = true;
+        inGame = true;
+    }
+
+
+    //aggiorna lo score andando avanti
+    public void Score()
+    {
+        if(inGame == true)
+        {
+            maxScore = score;
+
+            if(Input.GetKeyDown("up") || Input.GetButtonDown("Jump"))
+            {
+                score++;
+                textScore.text = "" + maxScore;
+            }
+            //else if (Input.GetKeyDown("down"))
+            //{
+            //    minScore--;
+            //    score = minScore;
+            //    maxScore != minScore;
+                
+            //    //maxScore = score;
+            //}
+        }
     }
 }
